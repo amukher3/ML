b273baa134e044776b4cc514749db317aaffef0f
amukher3 <51139088+amukher3@users.noreply.github.com>
2019-06-09 21:25:12 -0400

    Classifying recurrence of breast cancer
    This problem involved a lot of data cleaning task and required many pre-processing steps.

--- breast-cancer_data.py
+++ breast-cancer_data.py
@@ -73,37 +73,51 @@ df.iloc[:,1].unique()
 df=df.fillna(method='backfill')
 df.iloc[:,1].unique()
 
-
+#Label encoding the binary values
 label_binary = LabelEncoder()
 df.iloc[:,1]= label_binary.fit_transform(df.iloc[:,1])
 df.iloc[:,1].unique()
 
 df.iloc[:,1].unique()
+
+#one hot encoding for the feature variable
 one_hot = pd.get_dummies(df.iloc[:,2])
 df=pd.concat([df, one_hot], axis=1)
+
+# dropping to avoid dummy variable trap and dropping
+# the feature variable
 df.drop('3',axis=1,inplace=True)
 df.drop(3,axis=1,inplace=True)
-#df.drop('no',axis=1,inplace=True)
 
+#Label encoding the binary values
 label_binary = LabelEncoder()
 df.iloc[:,2]= label_binary.fit_transform(df.iloc[:,2])
 df.iloc[:,2].unique()
 
+
 df.iloc[:,3].unique()
+#one hot encoding for the feature variable
 one_hot = pd.get_dummies(df.iloc[:,3])
 df=pd.concat([df, one_hot], axis=1)
+
+# dropping to avoid dummy variable trap and dropping
+# the feature variable
 df.drop('left_low',axis=1,inplace=True)
 
+#Label encoding the binary values
 label_binary = LabelEncoder()
 df.iloc[:,3]= label_binary.fit_transform(df.iloc[:,3])
+
 df.iloc[:,3].unique()
 
+#Separating into X and y (features and labels)
 X=df.iloc[:,1:]
 y=df.iloc[:,1]
 
-
+#Separating into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
 
+# SVM classifier
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, y_train)
